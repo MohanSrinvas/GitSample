38952c954ca083ccc540671d07bccbe96dd92dfa
MohanSrinivas <mohansrinivas47@gmail.com>
2022-05-09 10:13:11 +0530

    New changes

--- src/org/sample/Emp.java
+++ src/org/sample/Emp.java
@@ -3,6 +3,6 @@ package org.sample;
 public class Emp {
   public static void main(String[] args) {
 	System.out.println("First change");
-	System.out.println("Emp change");
+	System.out.println("emp change");
 }
 }
